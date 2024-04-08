298791922fc9e9851affd7cf5db8ee68ee0438e0
Akihiro Fujihara <akihiro.fujihara@p.chibakoudai.jp>
2024-04-08 21:04:36 +0900

    clone factcheck dao

---
+++ web3_quiz/src/contract/config.jsx
@@ -0,0 +1,7 @@
+const chainId = "0x13881"; // (required) chainId to be used
+const rpc = "https://rpc-mumbai.polygon.technology/"; // (required for Ethereum) JSON RPC endpoint
+
+const quiz_address = "0xf4cb1169849F19dEE7C27Fa31759DEfB0A10afbc";
+const token_address = "0xb3bE509DDF865b66b22afeC37Eda3E4015528C43";
+
+export { chainId, rpc, quiz_address, token_address };
